b21897ec311ea5dd67038f62db589198a222f91f
Aymanch2764 <f223652@cfd.nu.edu.pk>
2024-10-10 10:11:05 +0500

    add the addition nd multiplication methods

---
+++ src/lab08/calculator.java
@@ -0,0 +1,27 @@
+package lab08;
+public class calculator 
+{   
+	
+ int num1,num2;
+	 int sum=0;
+
+	public calculator() 
+	{
+		
+	}
+
+	public static void main(String[] args) 
+	{
+		
+
+	}
+	public Integer addition(int num1,int num2)
+	{
+		return num1+num2;
+	}
+	public Integer multipilcation(int num1,int num2)
+	{
+		return num1*num2;
+	}
+
+}
